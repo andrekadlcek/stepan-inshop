8049df5090bece1ab55d5e16b5cc5d90dab1c2d1
Stepan Poul <poulstepan@gmail.com>
2022-03-23 13:27:03 +0100

    commit

---
+++ src/js/pages/cart/header-cart-item.svelte
@@ -0,0 +1,22 @@
+<script type="ts">
+
+</script>
+
+<tr class="header-cart-preview">
+    <td class="count">
+        (3x)
+    </td>
+    <td class="product">
+        BAUME &amp; MERCIER Linea 10008
+        
+        <br>
+        <strong>
+            
+                    97&nbsp;662&nbsp;Kč
+                    
+        </strong> 
+    </td>
+    <td class="remove">
+        <a href="scripts/shop.aspx?action=deletecartitem&amp;IDCartItem=256"><i class="text-icon icon-remove"></i></a> 
+    </td>
+</tr>
--- src/js/pages/cart/header-cart.svelte
+++ src/js/pages/cart/header-cart.svelte
@@ -1,78 +1,40 @@
 <script type="ts">
 
+import Item from '../cart/header-cart-item.svelte'
+import { completeData, cartData, cartGifts, cartStrings, clearCart, loadingCart } from "../../stores/cart-store";
+
 </script>
 
 
-                    <a href="/inshop/scripts/shop.aspx?action=showshoppingcart" class="inner bdr">
-                        <div class="is-icon icon-cart bdr">  </div> <!-- icon -->
-                        <div class="summary">
-                            <span class="monitor">
-                                <strong>Košík</strong>
-                                <span id="headerCartCount">2</span> kusů
-                                <span id="headerCartPrice" class="price">102&nbsp;422&nbsp;Kč</span>
-                            </span>
-                            <span class="device"><strong>2</strong></span>
-                        </div> <!-- summary -->
-                    </a> <!-- inner -->
-                    <div id="cartPreview" class="cart-preview">
-                        <table>
-                            <tbody>
-            
-                    <tr class="header-cart-preview">
-                        <td class="count">
-                            (3x)
-                        </td>
-                        <td class="product">
-                            BAUME &amp; MERCIER Linea 10008
-                            
-                            <br>
-                            <strong>
-                              
-                                        97&nbsp;662&nbsp;Kč
-                                     
-                            </strong> 
-                        </td>
-                        <td class="remove">
-                            <a href="scripts/shop.aspx?action=deletecartitem&amp;IDCartItem=256"><i class="text-icon icon-remove"></i></a> 
-                        </td>
-                    </tr>
-                
-                    <tr class="header-cart-preview">
-                        <td class="count">
-                            (5x)
-                        </td>
-                        <td class="product">
-                            883 Police Magna
-                            
-                                Velikost: S 
-                              
-                            <br>
-                            <strong>
-                              
-                                        4&nbsp;760&nbsp;Kč
-                                     
-                            </strong> 
-                        </td>
-                        <td class="remove">
-                            <a href="scripts/shop.aspx?action=deletecartitem&amp;IDCartItem=257"><i class="text-icon icon-remove"></i></a> 
-                        </td>
-                    </tr>
-                
-                    </tbody>
-                     <tfoot>
-                        <tr>
-                            <td></td>
-                            <td class="summprice" colspan="2">
-                                <span id="cartHeaderSummprice">102&nbsp;422&nbsp;Kč</span>
-                                <small>
-                                     <!-- CartPrice -->
-                                    <span id="cartHeaderSummpriceWithoutVat">84&nbsp;650&nbsp;Kč</span> (Celkem bez DPH)
-                                </small>
-                            </td>
-                        </tr>
-                         </tfoot>
-                        </table>
-                        <div class="go2cart">
-                            <a class="btn btn-primary large" href="/inshop/scripts/shop.aspx?action=showshoppingcart"><span>Košík</span></a>
-                        </div> <!-- go2cart -->
-                    </div> <!-- cart-preview -->
+<a href="/inshop/scripts/shop.aspx?action=showshoppingcart" class="inner bdr">
+    <div class="is-icon icon-cart bdr">  </div> <!-- icon -->
+    <div class="summary">
+        <span class="monitor">
+            <strong>{$cartStrings.Cart_Title}</strong>
+            <span id="headerCartCount">2</span> kusů
+            <span id="headerCartPrice" class="price">{$cartData.Cart_TotalPrice.price.PriceWithVat}</span>
+        </span>
+        <span class="device"><strong>2</strong></span>
+    </div> <!-- summary -->
+</a> <!-- inner -->
+<div id="cartPreview" class="cart-preview">
+    <table>
+        <tbody>
+            <Item />
+        </tbody>
+    <tfoot>
+        <tr>
+            <td></td>
+            <td class="summprice" colspan="2">
+                <span id="cartHeaderSummprice">{$cartData.Cart_TotalPrice.price.PriceWithVat}</span>
+                <small>
+                    <span id="cartHeaderSummpriceWithoutVat">{$cartData.Cart_TotalPrice.price.Price}</span> ({$cartStrings.Cart_SumPriceWithoutTax})
+                </small>
+            </td>
+        </tr>
+    </tfoot>
+    </table>
+    <div class="go2cart">
+        <a class="btn btn-primary large" href="/inshop/scripts/shop.aspx?action=showshoppingcart"><span>{$cartStrings.Cart_Title}</span></a>
+    </div> <!-- go2cart -->
+</div> <!-- cart-preview -->
